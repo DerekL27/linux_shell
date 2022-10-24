a42611bbfb1ba295bcb51a4382671cb5e4d288af
DerekL27 <dleung2140@gmail.com>
2022-10-24 19:47:47 -0400

    hi

--- shell.c
+++ shell.c
@@ -183,14 +183,14 @@ void handle_command(char cmd[]) {
 
         if(strstr(subcommands[i],"<")){
             char* tempRedirect[2];
-            split(subcommands[i],tempRedirect,"<");
+            split(subcommands[i],tempRedirect,"< ");
             close(0);
             assert(open(tempRedirect[1], O_RDONLY) != -1);
             subcommands[i] = tempRedirect[0];
         }
         if(strstr(subcommands[i],">")){
             char* tempRedirect[2];
-            split(subcommands[i],tempRedirect,">");
+            split(subcommands[i],tempRedirect,"> ");
             close(1);
             assert(open(tempRedirect[1], O_WRONLY | O_CREAT | O_TRUNC, 0644) != -1);
             subcommands[i] = tempRedirect[0];
